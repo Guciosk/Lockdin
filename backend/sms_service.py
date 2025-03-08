30863c211acac6eee64a7f73f2cc8caa45c5b6a3
Jeremy <jeremyd1818@gmail.com>
2025-03-07 21:22:19 -0500

    sms_service

---
+++ backend/sms_service.py
@@ -0,0 +1,28 @@
+from twilio.rest import Client
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# Load Twilio credentials from environment variables
+TWILIO_ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SID")
+TWILIO_AUTH_TOKEN = os.environ.get("TWILIO_AUTH_TOKEN")
+TWILIO_PHONE_NUMBER = os.environ.get("TWILIO_PHONE_NUMBER")
+
+# Initialize Twilio client
+client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+
+def send_sms(to_phone_number: int, body: str):
+    """
+    Sends an SMS using Twilio's API.
+
+    :param to_phone_number: Phone number to send the SMS to
+    :param body: The body content of the SMS
+    :return: The response from Twilio's API (message SID, etc.)
+    """
+    message = client.messages.create(
+        body=body,
+        from_=TWILIO_PHONE_NUMBER,
+        to=to_phone_number
+    )
+    return message.sid
